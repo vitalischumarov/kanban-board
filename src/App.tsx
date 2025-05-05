3430ced90354da7b9c79c10ae9940339aeca6284
Vitali Schumarov <v.schumarov@icloud.com>
2025-05-05 04:44:21 +0200

    drag and drop works fine

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,76 @@ import "./App.css";
 import Board from "./components/Board";
 import { DndContext, DragEndEvent } from "@dnd-kit/core";
 import { UniqueIdentifier } from "@dnd-kit/core";
+import { useState } from "react";
+import { taskType } from "./dataTypes/taskType";
 
 const boards: UniqueIdentifier[] = ["Backlog", "In Progress", "Done"];
 
+const allTasks = [
+  {
+    id: "1",
+    title: "Titel 1",
+    desctiption: "Description of task One",
+    status: "Backlog",
+  },
+  {
+    id: "2",
+    title: "Titel 2",
+    desctiption: "Description of task Two",
+    status: "In Progress",
+  },
+  {
+    id: "3",
+    title: "Titel 3",
+    desctiption: "Description of task Three",
+    status: "Done",
+  },
+  {
+    id: "4",
+    title: "Titel 4",
+    desctiption: "Description of task Four",
+    status: "In Progress",
+  },
+];
+
 function App() {
+  const [tasks, setTasks] = useState<taskType[]>(allTasks);
+
   function handleDragEnd(event: DragEndEvent) {
     const { over, active } = event;
     //Diese Aktion wird ausgefuehrt, wenn losgelassen wird!!!
     if (over && over.id === "Backlog") {
-      const newArray = 
+      let newArray = tasks.map((task) => {
+        if (task.id === active.id) {
+          task.status = "Backlog";
+        }
+        return;
+      });
+      setTasks([...tasks, newArray]);
     } else if (over && over.id === "Done") {
-      console.log("du bist bei Done");
-    } else console.log("du bist beim Im Progress");
+      let newArray = tasks.map((task) => {
+        if (task.id === active.id) {
+          task.status = "Done";
+        }
+        return;
+      });
+      setTasks([...tasks, newArray]);
+    } else {
+      let newArray = tasks.map((task) => {
+        if (task.id === active.id) {
+          task.status = "In Progress";
+        }
+        return;
+      });
+      setTasks([...tasks, newArray]);
+    }
   }
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
       <div className="bg-stone-900 h-screen flex justify-center gap-50">
         {boards.map((board) => {
-          return <Board boardStatus={board}></Board>;
+          return <Board boardStatus={board} tasks={tasks}></Board>;
         })}
       </div>
     </DndContext>
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,43 +1,14 @@
 import Task from "./Task";
-import { useState } from "react";
-import { taskType } from "../dataTypes/taskType";
 import { useDroppable } from "@dnd-kit/core";
 import { UniqueIdentifier } from "@dnd-kit/core";
+import { taskType } from "../dataTypes/taskType";
 
 type Prop = {
   boardStatus: UniqueIdentifier;
+  tasks: taskType[];
 };
 
-export default function Board({ boardStatus }: Prop) {
-  const allTasks = [
-    {
-      id: "1",
-      title: "Titel 1",
-      desctiption: "Description of task One",
-      status: "Backlog",
-    },
-    {
-      id: "2",
-      title: "Titel 2",
-      desctiption: "Description of task Two",
-      status: "In Progress",
-    },
-    {
-      id: "3",
-      title: "Titel 3",
-      desctiption: "Description of task Three",
-      status: "Done",
-    },
-    {
-      id: "4",
-      title: "Titel 4",
-      desctiption: "Description of task Four",
-      status: "In Progress",
-    },
-  ];
-
-  const [tasks, setTasks] = useState<taskType[]>(allTasks);
-
+export default function Board({ boardStatus, tasks }: Prop) {
   const { isOver, setNodeRef } = useDroppable({
     id: boardStatus,
   });
